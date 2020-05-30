17db1f0c97584b33c2b643c554d2d8f79b69bb7f
Luis Costa <luispcosta18@gmail.com>
2020-05-30 12:30:49 +0100

    Initial commit
    * Add initial files
    * Add dep files
    * Add `cobra` dep since its a nice utility tool to create CLI apps

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tt",
+	Short: "go-tt is a simple CLI app time tracker",
+	Long: `
+		With go-tt you can easily track the time you spend on different activities throughout your day. 
+		go-tt provides several reports outlining the time you have spent in all your registered activities.
+		The goal of this small app is to help you fight procrastination, by making you aware of where you chose to spend your time.
+	`,
+}
+
+// Execute executes the root commmand.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/luispcosta/go-tt/cmd"
+
+func main() {
+	cmd.Execute()
+}
